8e9084cffd8afcd00fca8b3fbc9c720f9687695d
Jon Rowe <hello@jonrowe.co.uk>
2016-07-21 20:43:58 +1000

    modernise specs

--- lib/warden/redirect.rb
+++ lib/warden/redirect.rb
@@ -1,6 +1,6 @@
 module Warden
   class Redirect < Array
-    def initialize(location,status=302,headers={})
+    def initialize(location, status = 302, headers = {})
       redirect_headers = headers.merge({ "Content-Type" => "text/html", "Location" => location })
       body = "<html><body>You are being redirected to <a href='#{location}'>#{location}</a></body></html>"
       super [status,redirect_headers,[body]]
--- spec/warden/redirect_spec.rb
+++ spec/warden/redirect_spec.rb
@@ -1,64 +1,61 @@
 require 'warden/redirect'
 
-shared_examples_for 'warden redirect' do |opts|
+RSpec.describe 'Warden::Redirect' do
+  shared_examples_for 'redirecting' do |opts|
+    it 'behaves like an array' do
+      status, headers, body = redirect_response
 
-  #rack status
-  specify { expect(subject[0]).to      eq opts[:status] }
-  specify { expect(subject.status).to  eq opts[:status] }
-  specify { expect(subject.first).to   eq opts[:status] }
+      expect(redirect_response).to be_a Array
+      expect(status).to eq opts[:status]
+      expect(headers).to eq opts[:headers]
+      expect(body).to include a_string_including 'You are being redirected'
+    end
 
-  #rack headers
-  specify { expect(subject[1]).to      eq opts[:headers] }
-  specify { expect(subject.headers).to eq opts[:headers] }
+    it 'exposes status' do
+      expect(redirect_response.status).to eq opts[:status]
+    end
 
-  #rack body
-  specify { expect(subject[2].first).to include 'You are being redirected' }
+    it 'exposes headers' do
+      expect(redirect_response.headers).to eq opts[:headers]
+    end
 
-  #warden compat
-  it { is_expected.to be_a Array }
-  specify do
-    status, headers, body = subject
-    expect(status).to  eq opts[:status]
-    expect(headers).to eq opts[:headers]
-    expect(body).to    eq subject[2]
-  end
-end
-
-describe 'redirecting to a specific location' do
-  subject { Warden::Redirect.new '/location' }
-
-  it_should_behave_like 'warden redirect',
-    :status  => 302,
-    :headers => { "Location" => "/location", "Content-Type" => "text/html" }
-end
-
-describe 'redirecting to a specific location with a status' do
-  subject { Warden::Redirect.new '/location', 301 }
-
-  it_should_behave_like 'warden redirect',
-    :status  => 301,
-    :headers => { "Location" => "/location", "Content-Type" => "text/html" }
-end
-
-describe 'redirecting to a specific location with a status and headers' do
-  subject { Warden::Redirect.new '/location', 301, "X-SHALL-NOT-PASS" => true  }
-
-  it_should_behave_like 'warden redirect',
-    :status  => 301,
-    :headers => { "Location" => "/location", "Content-Type" => "text/html", "X-SHALL-NOT-PASS" => true }
-end
-
-describe 'warden compatilbity' do
-  subject      { throw :warden, Warden::Redirect.new('/location') }
-  let(:result) { catch(:warden) { subject } }
-
-  specify do
-    case result
-    when Array
-      true
-    else
-      fail
+    it 'exposes the body' do
+      expect(redirect_response.body).to include a_string_including 'You are being redirected'
     end
   end
 
+  describe 'redirecting to a specific location' do
+    let(:redirect_response) { Warden::Redirect.new '/location' }
+
+    it_behaves_like 'redirecting',
+      :status  => 302,
+      :headers => { "Location" => "/location", "Content-Type" => "text/html" }
+  end
+
+  describe 'redirecting to a specific location with a status' do
+    let(:redirect_response) { Warden::Redirect.new '/location', 301 }
+
+    it_behaves_like 'redirecting',
+      :status  => 301,
+      :headers => { "Location" => "/location", "Content-Type" => "text/html" }
+  end
+
+  describe 'redirecting to a specific location with a status and headers' do
+    let(:redirect_response) { Warden::Redirect.new '/location', 301, "X-SHALL-NOT-PASS" => true  }
+
+    it_behaves_like 'redirecting',
+      :status  => 301,
+      :headers => { "Location" => "/location", "Content-Type" => "text/html", "X-SHALL-NOT-PASS" => true }
+  end
+
+  describe 'warden compatibility' do
+    let(:redirect_response) { throw :warden, Warden::Redirect.new('/location') }
+    let(:result) { catch(:warden) { redirect } }
+
+    it 'returns an array for compatibility with warden' do
+      expect(
+        catch(:warden) { throw :warden, Warden::Redirect.new('/location') }
+      ).to be_a Array
+    end
+  end
 end
